6b83f7d5e9674cc6e104eac29ac2810fd6b7da37
Peter <peter.pachov@hotmail.fr>
2021-12-22 00:02:44 +0200

    trying to add logo

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -36,3 +36,22 @@ html_theme = 'sphinx_rtd_theme'
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
+
+html_theme_options = {
+#    'analytics_id': 'G-XXXXXXXXXX',  #  Provided by Google in your dashboard
+#    'analytics_anonymize_ip': False,
+    'logo_only': False,
+    'html_logo': "logo_mail_sign.png",
+    'display_version': True,
+    'prev_next_buttons_location': 'bottom',
+#    'style_external_links': False,
+#    'vcs_pageview_mode': '',
+    'style_nav_header_background': '#2439bb',
+    # Toc options
+#    'collapse_navigation': True,
+#    'sticky_navigation': True,
+#    'navigation_depth': 4,
+#    'includehidden': True,
+#    'titles_only': False
+}
+
